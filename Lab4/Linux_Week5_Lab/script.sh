2da94d76e7194dcd7c33c7667b71283251a85d94
erichung1113 <erichung1113@gmail.com>
2023-10-12 22:57:27 +0800

    Bash and Shell Scripts

---
+++ Lab4/Linux_Week5_Lab/script.sh
@@ -0,0 +1,30 @@
+all_files=$(ls compressed_files)
+mkdir compressed_files/zip
+mkdir compressed_files/rar
+mkdir compressed_files/tar.gz
+mkdir compressed_files/unknown
+
+# uncompress
+for file_name in $all_files; do
+    if [[ $file_name =~ .zip$ ]]; then
+        unzip compressed_files/$file_name -d compressed_files/zip
+    elif [[ $file_name =~ .rar$ ]]; thenx
+        unrar x compressed_files/$file_name compressed_files/rar
+    elif [[ $file_name =~ .tar.gz$ ]]; then
+        tar -xzf compressed_files/$file_name -C compressed_files/tar.gz
+    else
+        mv compressed_files/$file_name compressed_files/unknown
+    fi
+done
+
+# wrong_list
+for student_id in $(ls compressed_files/unknown); do
+    echo $student_id | cut -c1-9 >> "wrong_list"
+done
+
+# missing_list
+for student_id in $(cat student_id); do
+    if !(echo $all_files | grep -q $student_id); then
+        echo $student_id >> "missing_list"
+    fi
+done
